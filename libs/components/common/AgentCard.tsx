f0c1dcac5b85f3ae5855933587678e7c0be09735
Alan <asilbekotanazarov05@dhu.ac.kr>
2026-09-23 22:57:39 +0900

    feat: develop agents list screen component logic

--- libs/components/common/AgentCard.tsx
+++ libs/components/common/AgentCard.tsx
@@ -12,10 +12,11 @@ import { userVar } from '../../../apollo/store';
 
 interface AgentCardProps {
 	agent: any;
+	likeMemberHandler: any;
 }
 
 const AgentCard = (props: AgentCardProps) => {
-	const { agent } = props;
+	const { agent, likeMemberHandler } = props;
 	const device = useDeviceDetect();
 	const user = useReactiveVar(userVar);
 	const imagePath: string = agent?.memberImage
@@ -26,7 +27,7 @@ const AgentCard = (props: AgentCardProps) => {
 		return <div>AGENT CARD</div>;
 	} else {
 		return (
-			<Stack className="agent-general-card">
+			<Stack className="agent-general-card" sx={{ width: '233px', height: '347px', flexShrink: 0 }}>
 				<Link
 					href={{
 						pathname: '/agent/detail',
@@ -64,7 +65,7 @@ const AgentCard = (props: AgentCardProps) => {
 							<RemoveRedEyeIcon />
 						</IconButton>
 						<Typography className="view-cnt">{agent?.memberViews}</Typography>
-						<IconButton color={'default'}>
+						<IconButton color={'default'} onClick={() => likeMemberHandler(user, agent?._id)}>
 							{agent?.meLiked && agent?.meLiked[0]?.myFavorite ? (
 								<FavoriteIcon color={'primary'} />
 							) : (
